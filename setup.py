24b43c2cf6abbf629b7d2db417c14ce157af7100
mjh <matthew-h@ar.sanken.osaka-u.ac.jp>
2021-02-18 17:55:16 +0900

    Added module setup file.

---
+++ setup.py
@@ -0,0 +1,16 @@
+'''Setup file for this module.'''
+
+## External modules.
+from setuptools import setup, find_packages
+
+
+###############################################################################
+
+
+## For package setup (in particular, pip install -e .).
+setup(name="sgd-roboost", version="1.0", packages=find_packages())
+# Reference:
+# https://stackoverflow.com/a/50194143
+
+
+###############################################################################
